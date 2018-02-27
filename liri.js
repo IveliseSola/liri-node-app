e91c37a83594b24ad72274f3556eacb06922387c
Ivelise Sola <solaivelise@yahoo.com>
2018-02-26 23:59:25 -0500

    write file function

--- liri.js
+++ liri.js
@@ -31,8 +31,9 @@ function main(parameter1, parameter2) {
 
 main(type, value);
 
-function getTweets() {
 
+function getTweets() {
+    var arrayToWrite = [];
     var parameters = {
         screen_name: "SolaIvelise",
         count: 20
@@ -40,43 +41,40 @@ function getTweets() {
     client.get("statuses/user_timeline", parameters, function (error, tweets, response) {
         if (error) throw error;
         for (i = 0; i < tweets.length; i++) {
-            console.log(" ");
-            console.log([i + 1] + ". " + tweets[i].text);
-            console.log("Created on: " + tweets[i].created_at);
-            console.log(" ");
-        }
-    });
+            console.log(" " + "\n" + [i + 1] + ". "
+                + tweets[i].text + "\n" + "Created on: "
+                + tweets[i].created_at + "\n");
 
+            arrayToWrite.push(" " + "\n" + [i + 1] + ". "
+                + tweets[i].text + "\n" + "Created on: "
+                + tweets[i].created_at + "\n");
+        }
+        writeThis(arrayToWrite);
+    });
 }
 
 function spotifySong(parameter) {
-
+    var arrayToWrite = [];
     if (!parameter) {
         parameter = "The Sign";
     }
-    // spotify.request("https://api.spotify.com/v1/search?q=" + parameter + "&type=track", function (error, response, body) {
-    //     if (!error && response.statusCode == 200) {
-    //         console.log(body);
-    // var result = JSON.parse(body);
-    // console.log(" ");
-    // console.log('Artist: ' + result.tracks.items[0].artists[0].name);
-    // console.log('Song: ' + result.tracks.items[0].name);
-    // console.log('Preview Link: ' + result.tracks.items[0].preview_url);
-    // console.log('Album: ' + result.tracks.items[0].album.name);
-    // console.log(" ");
-    // }
     spotify.search({ type: "track", query: parameter }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
         // console.log(data.tracks.items[0]);
-        console.log("");
+
         for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
-            console.log(data.tracks.items[0].artists[i].name);
+            console.log(" " + "\n" + data.tracks.items[0].artists[i].name + "\n");
+            arrayToWrite.push(" " + "\n" + data.tracks.items[0].artists[i].name + "\n");
         }
-        console.log(data.tracks.items[0].preview_url);
-        console.log(data.tracks.items[0].name);
-        console.log("");
+        console.log(" " + "\n" + data.tracks.items[0].preview_url + ". "
+            + data.tracks.items[0].name + "\n");
+
+        arrayToWrite.push(" " + "\n" + data.tracks.items[0].preview_url + ". "
+            + data.tracks.items[0].name + "\n");
+
+        writeThis(arrayToWrite);
     });
 }
 
@@ -84,39 +82,49 @@ function getValue(parameter) {
     var text = "";
     for (var i = 3; i < parameter.length; i++) {
 
-        // if (i > 3 && i < parameter.length) {
-        //     text = text + "+" + parameter[i];
-        // }
-        // else {
+        if (i > 3 && i < parameter.length) {
+            text = text + "+" + parameter[i];
+        }
+        else {
             text += parameter[i];
-        // }
+        }
     }
     return text;
 }
 
 function getMovie(parameter) {
+    var arrayToWrite = [];
     if (!parameter) {
         parameter = 'Mr. Nobody';
     }
     request("http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("");
             // console.log(JSON.parse(body));
-            console.log("The movie's Title is: " + JSON.parse(body).Title);
-            console.log("The movie's Year is: " + JSON.parse(body).Year);
-            console.log("The movie's Imdb Rating is: " + JSON.parse(body).imdbRating);
-            console.log("The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value);
-            console.log("The movie's Country is: " + JSON.parse(body).Country);
-            console.log("The movie's Language is: " + JSON.parse(body).Language);
-            console.log("The movie's Plot is: " + JSON.parse(body).Plot);
-            console.log("The movie's Actors are: " + JSON.parse(body).Actors);
-            console.log("");
+            console.log(" " + "\n" + "The movie's Title is: " + JSON.parse(body).Title + ". "
+                + "The movie's Year is: " + JSON.parse(body).Year + ". "
+                + "The movie's Imdb Rating is: " + JSON.parse(body).imdbRating + ". "
+                + "The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value + ". "
+                + "The movie's Country is: " + JSON.parse(body).Country + ". "
+                + "The movie's Language is: " + JSON.parse(body).Language + ". "
+                + "The movie's Plot is: " + JSON.parse(body).Plot + ". "
+                + "The movie's Actors are: " + JSON.parse(body).Actors + "\n");
+
+            arrayToWrite.push(" " + "\n" + "The movie's Title is: " + JSON.parse(body).Title + ". "
+                + "The movie's Year is: " + JSON.parse(body).Year + ". "
+                + "The movie's Imdb Rating is: " + JSON.parse(body).imdbRating + ". "
+                + "The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value + ". "
+                + "The movie's Country is: " + JSON.parse(body).Country + ". "
+                + "The movie's Language is: " + JSON.parse(body).Language + ". "
+                + "The movie's Plot is: " + JSON.parse(body).Plot + ". "
+                + "The movie's Actors are: " + JSON.parse(body).Actors + "\n")
+
+            writeThis(arrayToWrite);
         }
     });
 }
 
 function extFile() {
-    
+
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
@@ -124,13 +132,24 @@ function extFile() {
         var pos = data.indexOf(",");
         //console.log(pos);
         type = data.substr(0, pos);
-        console.log(type);
+        //console.log(type);
         var str = data.substr(pos + 1);
         str.replace(" ", "+");
-        console.log(str);
+        //console.log(str);
 
         main(type, str);
     });
+}
+
+function writeThis(parameter) {
+
+    for (var i = 0; i < parameter.length; i++) {
+        fs.appendFile("log.txt", parameter[i], function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
 }
 
 
